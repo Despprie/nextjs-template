8ffe6f497926065ce4c0f453c234cd007bd98ff9
Fabien Lallemand <fabien-luc.lallemand@epitech.eu>
2022-05-01 19:02:42 +0400

    setup nextjs repository

---
+++ src/pages/_app.tsx
@@ -0,0 +1,22 @@
+import type { AppProps } from 'next/app';
+import { useRef } from 'react';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+import 'styles/globals.css';
+import 'styles/tailwind.css';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+    const queryClient = useRef(new QueryClient());
+
+    return (
+        <QueryClientProvider client={queryClient.current}>
+            <Hydrate state={pageProps.dehydratedState}>
+                <Component {...pageProps} />
+                {process.env.NEXT_PUBLIC_ENVIRONMENT === 'development' ? <ReactQueryDevtools /> : null}
+            </Hydrate>
+        </QueryClientProvider>
+    );
+};
+
+export default MyApp;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ['./src/**/*.{html,js,tsx,ts,tsx}'],
+    darkMode: 'class',
+    theme: {
+        extend: {}
+    },
+    plugins: []
+};
